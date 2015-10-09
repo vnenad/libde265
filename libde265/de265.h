dde4582054f458ffd494c57e8f8f6fe6ed36f6ce
Nenad Vasiljevic <vnenad@yahoo.com>
2015-10-09 11:03:31 +0200

    Visual Studio solution and build adjustments

--- libde265/de265.h
+++ libde265/de265.h
@@ -17,7 +17,7 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with libde265.  If not, see <http://www.gnu.org/licenses/>.
 */
-
+#define LIBDE265_STATIC_BUILD	//@@ ADDED
 
 #ifndef DE265_H
 #define DE265_H
